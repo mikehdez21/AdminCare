c02bb6a2fc505b566538d5143243147e14c74803
DesarrolloHSS <desarrollo@hospitalsanserafin.com>
2026-03-12 14:58:44 -0600

    Fix VercelJson Build

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+
+export default [
+  {
+    files: ['resources/js/**/*.{js,jsx,ts,tsx}', 'vite.config.ts'],
+    languageOptions: { globals: {...globals.browser, ...globals.node} },
+    rules: {
+      indent: ['error', 2], // Indentación de 2 espacios
+      quotes: ['error', 'single'], // Usar comillas simples
+      semi: ['off'], // Requerir punto y coma
+
+    },
+
+    ignores: [
+      'vendor/**',
+      'node_modules/**',
+      'public/build/**',
+      'storage/**',
+      'bootstrap/cache/**',
+      'database/**',
+      'dist/**',
+    ],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
+
+
