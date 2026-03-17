87c52ccaa8e19aa1e549b2c0610310f1e3286bbf
DesarrolloHSS <desarrollo@hospitalsanserafin.com>
2026-03-17 08:21:27 -0600

    Fix | Sessions

--- database/migrations/011_Laravel_create_Sessions_table.php
+++ database/migrations/011_Laravel_create_Sessions_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->constrained('tableUsuarios', 'id_usuario')->nulleable();
+            $table->foreignId('user_id')->constrained('tableUsuarios', 'id_usuario')->nullable();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
